061191cc8463b17d73b5c41fe6dc5cfbf0084536
Laszlo Hornyak <hornyak.laszlo88@gmail.com>
2021-06-01 15:12:57 +0200

    login and forgot password

---
+++ pages/forgot-password.vue
@@ -0,0 +1,88 @@
+<template>
+  <section class="section hero is-fullheight is-error-section">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-three-fifths">
+            <div class="card has-card-header-background">
+              <header class="card-header">
+                <p class="card-header-title">
+                  <span class="icon"><i class="mdi mdi-lock default" /></span>
+                  <span>Elfelejtett jelszó</span>
+                </p>
+              </header>
+              <div class="card-content">
+                <form @submit.prevent="submit">
+                  <b-field label="Email cím">
+                    <b-input v-model="email" type="email" required />
+                  </b-field>
+                  <hr>
+                  <div class="field">
+                    <div class="field-body">
+                      <div class="field columns">
+                        <div class="column is-half">
+                          <button type="submit" class="button is-fullwidth is-primary" :class="{'is-loading': isLoading}">
+                            Jelszó visszaállítása
+                          </button>
+                        </div>
+                        <div class="column is-half">
+                          <nuxt-link to="/login" class="button is-fullwidth is-outlined is-primary" :class="{'is-loading': isLoading}">
+                            Vissza
+                          </nuxt-link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'unauthorized',
+  data () {
+    return {
+      isLoading: false,
+      email: ''
+    }
+  },
+  methods: {
+    async submit () {
+      this.isLoading = true
+      try {
+        await this.$strapi.forgotPassword({
+          email: this.email
+        })
+        this.isLoading = false
+
+        this.$buefy.toast.open({
+          message: 'A jelszavad visszaállításához szükséges linket elküldtük az email címedre',
+          type: 'is-success',
+          queue: false
+        })
+
+        this.$router.push('/login')
+      } catch (err) {
+        this.isLoading = false
+
+        this.$buefy.toast.open({
+          message: 'A jelszavad visszaállításához szükséges linket elküldtük az email címedre',
+          type: 'is-success',
+          queue: false
+        })
+
+        this.$router.push('/login')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,88 @@
+<template>
+  <section class="section hero is-fullheight is-error-section">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-three-fifths">
+            <div class="card has-card-header-background">
+              <header class="card-header">
+                <p class="card-header-title">
+                  <span class="icon"><i class="mdi mdi-lock default" /></span>
+                  <span>Belépés</span>
+                </p>
+              </header>
+              <div class="card-content">
+                <form @submit.prevent="submit">
+                  <b-field label="Email cím">
+                    <b-input v-model="email" type="email" required />
+                  </b-field>
+                  <b-field label="Jelszó">
+                    <b-input v-model="password" type="password" required />
+                  </b-field>
+                  <hr>
+                  <div class="field">
+                    <div class="field-body">
+                      <div class="field columns">
+                        <div class="column is-half">
+                          <button type="submit" class="button is-fullwidth is-primary" :class="{'is-loading': isLoading}">
+                            Bejelentkezés
+                          </button>
+                        </div>
+                        <div class="column is-half">
+                          <nuxt-link to="/forgot-password" class="button is-fullwidth is-outlined is-primary" :class="{'is-loading': isLoading}">
+                            Elfelejtett a jelszó
+                          </nuxt-link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'unauthorized',
+  data () {
+    return {
+      isLoading: false,
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async submit () {
+      this.isLoading = true
+      try {
+        await this.$strapi.login({
+          identifier: this.email,
+          password: this.password
+        })
+        this.isLoading = false
+
+        this.$router.push('/')
+      } catch (err) {
+        this.isLoading = false
+
+        const error = (err && err.message) ? err.message : ''
+        const message = (error === 'Identifier or password invalid.') ? 'Hibás email cím vagy jelszó' : 'Hiba történt'
+
+        this.$buefy.toast.open({
+          message,
+          type: 'is-danger',
+          queue: false
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
