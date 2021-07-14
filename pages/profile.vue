7cb4dbd962ba01a032e7f931ac4269a26b732b07
nuke7 <gombos.marton@icloud.com>
2021-07-14 18:36:03 +0200

    eslint-prettier setup

--- pages/profile.vue
+++ pages/profile.vue
@@ -5,15 +5,31 @@
     <hero-bar> Profil </hero-bar>
     <section class="section is-main-section">
       <tiles>
-        <card-component title="Profil" icon="id-card" class="tile is-child">
+        <card-component
+          title="Profil"
+          icon="id-card"
+          class="tile is-child"
+        >
           <b-field label="Felhasználónév">
-            <b-input :value="username" custom-class="is-static" readonly />
+            <b-input
+              :value="username"
+              custom-class="is-static"
+              readonly
+            />
           </b-field>
           <b-field label="Email cím">
-            <b-input :value="email" custom-class="is-static" readonly />
+            <b-input
+              :value="email"
+              custom-class="is-static"
+              readonly
+            />
           </b-field>
           <b-field label="Jogosultság">
-            <b-input :value="role" custom-class="is-static" readonly />
+            <b-input
+              :value="role"
+              custom-class="is-static"
+              readonly
+            />
           </b-field>
         </card-component>
       </tiles>
@@ -30,7 +46,9 @@
             <b-field label="Jelszó megerősítése">
               <b-input type="password" />
             </b-field>
-            <b-button type="is-success is-light"> Beállítás </b-button>
+            <b-button type="is-success is-light">
+              Beállítás
+            </b-button>
           </form>
         </card-component>
       </tiles>
@@ -39,35 +57,35 @@
 </template>
 
 <script>
-import CardComponent from "@/components/common/CardComponent";
-import HeroBar from "@/components/common/HeroBar";
-import Tiles from "@/components/common/Tiles";
+import CardComponent from '@/components/common/CardComponent'
+import HeroBar from '@/components/common/HeroBar'
+import Tiles from '@/components/common/Tiles'
 
 export default {
-  name: "Profile",
+  name: 'Profile',
   components: {
     Tiles,
     HeroBar,
-    CardComponent,
+    CardComponent
   },
-  data() {
-    return {};
+  data () {
+    return {}
   },
-  head() {
+  head () {
     return {
-      title: "Profil",
-    };
+      title: 'Profil'
+    }
   },
   computed: {
-    username() {
-      return this.$strapi.user ? this.$strapi.user.username : "";
+    username () {
+      return this.$strapi.user ? this.$strapi.user.username : ''
     },
-    email() {
-      return this.$strapi.user ? this.$strapi.user.email : "";
+    email () {
+      return this.$strapi.user ? this.$strapi.user.email : ''
     },
-    role() {
-      return this.$strapi.user ? this.$strapi.user.role.name : "";
-    },
-  },
-};
+    role () {
+      return this.$strapi.user ? this.$strapi.user.role.name : ''
+    }
+  }
+}
 </script>
