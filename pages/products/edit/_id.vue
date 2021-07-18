1906dc3d278de83e3550fb1b0f4ac0a8b50a4a77
nuke7 <gombos.marton@icloud.com>
2021-07-18 19:22:33 +0200

    products work in progress

---
+++ pages/products/edit/_id.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <hero-bar>
+      Termékek
+      <nuxt-link
+        slot="right"
+        to="/client"
+        class="button"
+      >
+        Termék hozzáadása
+      </nuxt-link>
+    </hero-bar>
+    <section class="section is-main-section">
+      <card-component
+        class="has-table"
+        title="Termékek"
+        icon="tags"
+      >
+        <data-table
+          :fields="fields"
+          :collection="collection"
+          :checkable="false"
+        />
+      </card-component>
+    </section>
+  </div>
+</template>
+
+<script>
+import HeroBar from '@/components/common/HeroBar'
+import CardComponent from '@/components/common/CardComponent'
+import DataTable from '@/components/DataTable'
+export default {
+  components: {
+    HeroBar,
+    DataTable,
+    CardComponent
+  },
+  data () {
+    return {
+      collection: 'products',
+      fields: [{
+        field: 'name',
+        title: 'Név'
+      }, {
+        field: 'grossPrice',
+        title: 'Bruttó ár'
+      }]
+    }
+  },
+  head () {
+    return {
+      title: 'Termékek'
+    }
+  },
+  computed: {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
