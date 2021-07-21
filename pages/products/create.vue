2d3f5dc3d080ea8fe3440c34e6a21a28c63fc2ab
nuke7 <gombos.marton@icloud.com>
2021-07-21 21:43:34 +0200

    creating product form works - period mandatory

---
+++ pages/products/create.vue
@@ -0,0 +1,202 @@
+<template>
+  <div>
+    <hero-bar>
+      Termék Hozzáadása
+      <nuxt-link
+        slot="right"
+        to="/products"
+        class="button"
+      >
+        Vissza a termékekhez
+      </nuxt-link>
+    </hero-bar>
+    <section class="section is-main-section">
+      <card-component
+        class="tile is-child"
+        title="Új termék"
+        icon="tags"
+      >
+        <form @submit.prevent="submit">
+          <b-field
+            label="Név"
+            message="A termék neve"
+            horizontal
+          >
+            <b-input
+              v-model="product.name"
+              required
+            />
+          </b-field>
+          <b-field
+            label="Leírás"
+            message="A termék leírása"
+            horizontal
+          >
+            <b-input
+              v-model="product.description"
+              required
+            />
+          </b-field>
+          <b-field
+            label="Bruttó ár"
+            message="A termék bruttó ára"
+            horizontal
+          >
+            <b-input
+              v-model="product.grossPrice"
+              required
+            />
+          </b-field>
+          <b-field
+            label="Számlázási név"
+            message="A termék számlázási neve"
+            horizontal
+          >
+            <b-input
+              v-model="product.nameInvoice"
+              required
+            />
+          </b-field>
+          <b-field
+            label="Szállítható-e"
+            message="A termék szállítható?"
+            horizontal
+          >
+            <b-select
+              v-model="product.isShippable"
+              required
+            >
+              <option :value="false">
+                Nem
+              </option>
+              <option :value="true">
+                Igen
+              </option>
+            </b-select>
+          </b-field>
+          <b-field
+            label="Egyszeri vagy előfizetés"
+            message="A terméknek egyszeri ára van, vagy előfizetéses?"
+            horizontal
+          >
+            <b-select
+              v-model="product.type"
+              required
+            >
+              <option :value="'one_time'">
+                Egyszeri
+              </option>
+              <option :value="'subscription'">
+                Előfizetéses
+              </option>
+            </b-select>
+          </b-field>
+          <b-field
+            v-if="subscription"
+            label="Előfizetés gyakorisága"
+            message="Milyen gyakran kell fizetni az előfizetést"
+            horizontal
+          >
+            <b-select
+              v-model="product.period"
+              required
+            >
+              <option :value="'weekly'">
+                Heti
+              </option>
+              <option :value="'monthly'">
+                Havi
+              </option>
+              <option :value="'yearly'">
+                Éves
+              </option>
+            </b-select>
+          </b-field>
+          <hr>
+          <b-field horizontal>
+            <b-button
+              type="is-primary"
+              :loading="isLoading"
+              native-type="submit"
+            >
+              Mentés
+            </b-button>
+            <nuxt-link
+              to="/products"
+              class="button is-secondary"
+            >
+              Vissza
+            </nuxt-link>
+          </b-field>
+        </form>
+      </card-component>
+    </section>
+  </div>
+</template>
+
+<script>
+import HeroBar from '@/components/common/HeroBar'
+import CardComponent from '@/components/common/CardComponent'
+export default {
+  components: {
+    HeroBar,
+    CardComponent
+  },
+  data () {
+    return {
+      isLoading: false,
+      product: {
+        name: '',
+        description: '',
+        nameInvoice: '',
+        grossPrice: '',
+        isShippable: false,
+        type: '',
+        period: ''
+      }
+    }
+  },
+  head () {
+    return {
+      title: 'Termék hozzáadása'
+    }
+  },
+  computed: {
+    subscription () {
+      return this.product.type === 'subscription'
+    }
+  },
+  methods: {
+    async submit () {
+      try {
+        this.isLoading = true
+
+        await this.$strapi.create('products', this.product)
+
+        this.isLoading = false
+        /* this.$buefy.toast.open({
+          message: 'Sikeresen mentve',
+          type: 'is-primary'
+          queue: false
+        }) */
+        this.$buefy.snackbar.open({
+          message: 'Sikeresen mentve',
+          type: 'is-white has-text-white has-background-primary',
+          queue: false
+        })
+        this.$router.push('/products')
+      } catch (err) {
+        this.isLoading = false
+        this.$buefy.toast.open({
+          message: `Error: ${err.message}`,
+          type: 'is-danger',
+          queue: false
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
