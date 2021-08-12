159184a4d9f2865c4db63f8bba35ca9d5770d6a3
nuke7 <gombos.marton@icloud.com>
2021-08-12 23:16:13 +0200

    Vobserver added

--- pages/purchases/edit/_id.vue
+++ pages/purchases/edit/_id.vue
@@ -16,323 +16,334 @@
         icon="shopping-basket"
         class="tile is-child"
       >
-        <form @submit.prevent="submit">
-          <b-field
-            label="Név"
-            message="Vásárló neve"
-            horizontal
-          >
-            <b-input
-              v-model="purchase.firstName"
-              placeholder="Keresztnév"
-              required
-            />
-            <b-input
-              v-model="purchase.lastName"
-              placeholder="Vezetéknév"
-              required
-            />
-          </b-field>
-          <b-field
-            label="Email"
-            message="Vásárló e-mail címe"
-            horizontal
-          >
-            <b-input
-              v-model="purchase.email"
-              required
-            />
-          </b-field>
-          <b-field
-            label="Hírlevélre feliratkozott"
-            message="A vásárló feliratkozott a hírlevélre"
-            horizontal
-          >
-            <b-select
-              v-model="purchase.isNewsletterSubscribed"
-              required
+        <ValidationObserver v-slot="{ invalid }">
+          <form @submit.prevent="submit">
+            <b-field
+              label="Név"
+              message="Vásárló neve"
+              horizontal
             >
-              <option :value="true">
-                igen
-              </option>
-              <option :value="false">
-                nem
-              </option>
-            </b-select>
-          </b-field>
-          <b-field
-            label="Feltételeket elfogadta"
-            message="A vásárló elfogadta az általános feltételeket"
-            horizontal
-          >
-            <b-select
-              v-model="purchase.isGTCaccepted"
-              required
-            >
-              <option :value="true">
-                igen
-              </option>
-              <option :value="false">
-                nem
-              </option>
-            </b-select>
-          </b-field>
-          <b-field
-            label="Adatkezelést elfogadta"
-            message="A vásárló elfogadta az adatkezelési feltételeket"
-            horizontal
-          >
-            <b-select
-              v-model="purchase.isPrivacyPolicyAccepted"
-              required
-            >
-              <option :value="true">
-                igen
-              </option>
-              <option :value="false">
-                nem
-              </option>
-            </b-select>
-          </b-field>
-          <b-field
-            label="Telefonszám"
-            message="Vásárló telefonszáma"
-            horizontal
-          >
-            <b-input
-              v-model="purchase.phoneNumber"
-              required
-            />
-          </b-field>
-          <b-field
-            label="Kiszállítási cím"
-            message="Ahova a terméket szállítani kell"
-            horizontal
-          >
-            <b-input
-              v-model="purchase.deliveryCountry"
-              placeholder="Ország"
-              required
-            />
-            <b-input
-              v-model="purchase.deliveryZip"
-              placeholder="Irányítószám"
-              required
-            />
-            <b-input
-              v-model="purchase.deliveryCity"
-              placeholder="Város"
-              required
-            />
-            <b-input
-              v-model="purchase.deliveryStreetNo"
-              placeholder="Utca és házszám"
-              required
-            />
-          </b-field>
-          <b-field
-            label="Számlázási cím"
-            message="A számlán szereplő cím"
-            horizontal
-          >
-            <b-input
-              v-model="purchase.invoiceCountry"
-              placeholder="Ország"
-              required
-            />
-            <b-input
-              v-model="purchase.invoiceZip"
-              placeholder="Irányítószám"
-              required
-            />
-            <b-input
-              v-model="purchase.invoiceCity"
-              placeholder="Város"
-              required
-            />
-            <b-input
-              v-model="purchase.invoiceStreetNo"
-              placeholder="Utca és házszám"
-              required
-            />
-          </b-field>
-          <b-field
-            label="Rendelés összege"
-            message="Mennyiért vásárolt a vevő"
-            horizontal
-          >
-            <b-input
-              v-model="purchase.sumOfPurchase"
-              required
-            />
-          </b-field>
-          <b-field
-            label="Fizetési mód"
-            message="Hogy fizet a vevő"
-            horizontal
-          >
-            <b-select
-              v-model="purchase.paymentMethod"
-              required
-            >
-              <option value="transfer">
-                Átutalás
-              </option>
-              <option value="creditCard">
-                Bankkártya
-              </option>
-            </b-select>
-          </b-field>
-          <b-field
-            label="Rendelés státusza"
-            message="Hol tart a rendelés"
-            horizontal
-          >
-            <b-select
-              v-model="purchase.status"
-              required
-            >
-              <option value="ordered">
-                Megrendelve
-              </option>
-              <option value="payed">
-                Kifizetve
-              </option>
-              <option value="shipped">
-                Kiszállítva
-              </option>
-            </b-select>
-          </b-field>
-          <hr>
-          <b-field
-            horizontal
-            label="Rendelt termékek"
-          >
-            Vásárló által rendelt termékek listája:
-          </b-field>
-          <b-collapse
-            v-for="(product, index) of purchase.products"
-            :key="index"
-            style="max-width: 80%; margin-left: 19%;"
-            class="card"
-            animation="slide"
-          >
-            <template #trigger="props">
-              <div
-                class="card-header"
-                role="button"
-              >
-                <p class="card-header-title">
-                  {{ product.name }}
-                </p>
-                <a class="card-header-icon">
-                  <b-icon :icon="props.open ? 'menu-up' : 'menu-down'" />
-                </a>
-              </div>
-            </template>
-            <div class="card-content">
-              <div class="content">
-                <p>
-                  <strong>Termék leírása:</strong>
-                  {{ product.description }}
-                </p>
-              </div>
-              <div class="content">
-                <p>
-                  <strong>Termék ára:</strong>
-                  <b-input
-                    v-model="product.grossPrice"
-                    required
-                  />
-                </p>
-              </div>
-              <div class="content">
-                <p>
-                  <strong>Rendelt mennyiség:</strong>
-                  <b-input
-                    v-model="product.quantity"
-                    type="number"
-                    required
-                  />
-                </p>
-              </div>
-            </div>
-            <footer class="card-footer">
-              <b-button
-                style="border-top-left-radius: 0; border-top-right-radius: 0; border-color: whitesmoke;"
-                label="Termék törlése"
-                icon-pack="fas"
-                icon-left="trash-alt"
-                class="card-footer-item has-text-danger"
-                @click="deleteProduct(index)"
+              <b-input
+                v-model="purchase.firstName"
+                placeholder="Keresztnév"
+                required
               />
-            </footer>
-          </b-collapse>
-          <b-field
-            v-if="!addProduct"
-            horizontal
-          >
-            <b-button
-              outlined
-              type="is-primary"
-              label="Termék hozzáadása"
-              :loading="isLoading"
-              @click="addProduct = true"
-            />
-          </b-field>
-          <b-field
-            v-else
-            horizontal
-          >
-            <strong>Termék:</strong>
-            <b-select
-              v-model="plusProductId"
-              required
+              <b-input
+                v-model="purchase.lastName"
+                placeholder="Vezetéknév"
+                required
+              />
+            </b-field>
+            <b-field
+              label="Email"
+              message="Vásárló e-mail címe"
+              horizontal
             >
-              <option
-                v-for="product in allProducts"
-                :key="product.id"
-                :value="product.id"
+              <ValidationProvider
+                v-slot="{ errors }"
+                name="Email"
+                rules="required|email"
               >
-                {{ product.name }}
-              </option>
-            </b-select>
-            <strong>Mennyiség:</strong>
-            <b-input
-              v-model="plusProductQuantity"
-              type="number"
-              required
-            />
-            <b-button
-              style="border-radius: 5px"
-              type="is-primary"
-              label="Hozzáadás"
-              size="is-small"
-              :loading="isLoading"
-              @click="addNewProduct()"
-            />
-            <b-button
-              style="border-radius: 5px"
-              type="is-danger"
-              label="Mégse"
-              size="is-small"
-              :loading="isLoading"
-              @click="addProduct = false"
-            />
-          </b-field>
-
-          <hr>
-          <b-field horizontal>
-            <b-button
-              type="is-primary"
-              :loading="isLoading"
-              native-type="submit"
-              expanded
+                <b-input
+                  v-model="purchase.email"
+                  type="email"
+                  required
+                />
+                <span class="has-text-danger is-size-7">{{ errors[0] }}</span>
+              </ValidationProvider>
+            </b-field>
+            <b-field
+              label="Hírlevélre feliratkozott"
+              message="A vásárló feliratkozott a hírlevélre"
+              horizontal
             >
-              Mentés
-            </b-button>
-          </b-field>
-        </form>
+              <b-select
+                v-model="purchase.isNewsletterSubscribed"
+                required
+              >
+                <option :value="true">
+                  igen
+                </option>
+                <option :value="false">
+                  nem
+                </option>
+              </b-select>
+            </b-field>
+            <b-field
+              label="Feltételeket elfogadta"
+              message="A vásárló elfogadta az általános feltételeket"
+              horizontal
+            >
+              <b-select
+                v-model="purchase.isGTCaccepted"
+                required
+              >
+                <option :value="true">
+                  igen
+                </option>
+                <option :value="false">
+                  nem
+                </option>
+              </b-select>
+            </b-field>
+            <b-field
+              label="Adatkezelést elfogadta"
+              message="A vásárló elfogadta az adatkezelési feltételeket"
+              horizontal
+            >
+              <b-select
+                v-model="purchase.isPrivacyPolicyAccepted"
+                required
+              >
+                <option :value="true">
+                  igen
+                </option>
+                <option :value="false">
+                  nem
+                </option>
+              </b-select>
+            </b-field>
+            <b-field
+              label="Telefonszám"
+              message="Vásárló telefonszáma"
+              horizontal
+            >
+              <b-input
+                v-model="purchase.phoneNumber"
+                required
+              />
+            </b-field>
+            <b-field
+              label="Kiszállítási cím"
+              message="Ahova a terméket szállítani kell"
+              horizontal
+            >
+              <b-input
+                v-model="purchase.deliveryCountry"
+                placeholder="Ország"
+                required
+              />
+              <b-input
+                v-model="purchase.deliveryZip"
+                placeholder="Irányítószám"
+                required
+              />
+              <b-input
+                v-model="purchase.deliveryCity"
+                placeholder="Város"
+                required
+              />
+              <b-input
+                v-model="purchase.deliveryStreetNo"
+                placeholder="Utca és házszám"
+                required
+              />
+            </b-field>
+            <b-field
+              label="Számlázási cím"
+              message="A számlán szereplő cím"
+              horizontal
+            >
+              <b-input
+                v-model="purchase.invoiceCountry"
+                placeholder="Ország"
+                required
+              />
+              <b-input
+                v-model="purchase.invoiceZip"
+                placeholder="Irányítószám"
+                required
+              />
+              <b-input
+                v-model="purchase.invoiceCity"
+                placeholder="Város"
+                required
+              />
+              <b-input
+                v-model="purchase.invoiceStreetNo"
+                placeholder="Utca és házszám"
+                required
+              />
+            </b-field>
+            <b-field
+              label="Rendelés összege"
+              message="Mennyiért vásárolt a vevő"
+              horizontal
+            >
+              <b-input
+                v-model="purchase.sumOfPurchase"
+                required
+              />
+            </b-field>
+            <b-field
+              label="Fizetési mód"
+              message="Hogy fizet a vevő"
+              horizontal
+            >
+              <b-select
+                v-model="purchase.paymentMethod"
+                required
+              >
+                <option value="transfer">
+                  Átutalás
+                </option>
+                <option value="creditCard">
+                  Bankkártya
+                </option>
+              </b-select>
+            </b-field>
+            <b-field
+              label="Rendelés státusza"
+              message="Hol tart a rendelés"
+              horizontal
+            >
+              <b-select
+                v-model="purchase.status"
+                required
+              >
+                <option value="ordered">
+                  Megrendelve
+                </option>
+                <option value="payed">
+                  Kifizetve
+                </option>
+                <option value="shipped">
+                  Kiszállítva
+                </option>
+              </b-select>
+            </b-field>
+            <hr>
+            <b-field
+              horizontal
+              label="Rendelt termékek"
+            >
+              Vásárló által rendelt termékek listája:
+            </b-field>
+            <b-collapse
+              v-for="(product, index) of purchase.products"
+              :key="index"
+              style="max-width: 80%; margin-left: 19%;"
+              class="card"
+              animation="slide"
+            >
+              <template #trigger="props">
+                <div
+                  class="card-header"
+                  role="button"
+                >
+                  <p class="card-header-title">
+                    {{ product.name }}
+                  </p>
+                  <a class="card-header-icon">
+                    <b-icon :icon="props.open ? 'menu-up' : 'menu-down'" />
+                  </a>
+                </div>
+              </template>
+              <div class="card-content">
+                <div class="content">
+                  <p>
+                    <strong>Termék leírása:</strong>
+                    {{ product.description }}
+                  </p>
+                </div>
+                <div class="content">
+                  <p>
+                    <strong>Termék ára:</strong>
+                    <b-input
+                      v-model="product.grossPrice"
+                      required
+                    />
+                  </p>
+                </div>
+                <div class="content">
+                  <p>
+                    <strong>Rendelt mennyiség:</strong>
+                    <b-input
+                      v-model="product.quantity"
+                      type="number"
+                      required
+                    />
+                  </p>
+                </div>
+              </div>
+              <footer class="card-footer">
+                <b-button
+                  style="border-top-left-radius: 0; border-top-right-radius: 0; border-color: whitesmoke;"
+                  label="Termék törlése"
+                  icon-pack="fas"
+                  icon-left="trash-alt"
+                  class="card-footer-item has-text-danger"
+                  @click="deleteProduct(index)"
+                />
+              </footer>
+            </b-collapse>
+            <b-field
+              v-if="!addProduct"
+              horizontal
+            >
+              <b-button
+                outlined
+                type="is-primary"
+                label="Termék hozzáadása"
+                :loading="isLoading"
+                @click="addProduct = true"
+              />
+            </b-field>
+            <b-field
+              v-else
+              horizontal
+            >
+              <strong>Termék:</strong>
+              <b-select
+                v-model="plusProductId"
+                required
+              >
+                <option
+                  v-for="product in allProducts"
+                  :key="product.id"
+                  :value="product.id"
+                >
+                  {{ product.name }}
+                </option>
+              </b-select>
+              <strong>Mennyiség:</strong>
+              <b-input
+                v-model="plusProductQuantity"
+                type="number"
+                required
+              />
+              <b-button
+                style="border-radius: 5px"
+                type="is-primary"
+                label="Hozzáadás"
+                size="is-small"
+                :loading="isLoading"
+                @click="addNewProduct()"
+              />
+              <b-button
+                style="border-radius: 5px"
+                type="is-danger"
+                label="Mégse"
+                size="is-small"
+                :loading="isLoading"
+                @click="addProduct = false"
+              />
+            </b-field>
+
+            <hr>
+            <b-field horizontal>
+              <b-button
+                type="is-primary"
+                :loading="isLoading"
+                native-type="submit"
+                expanded
+                :disabled="invalid"
+              >
+                Mentés
+              </b-button>
+            </b-field>
+          </form>
+        </ValidationObserver>
       </card-component>
     </section>
   </div>
