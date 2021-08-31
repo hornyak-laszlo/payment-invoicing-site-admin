df56f6bc0c26e26244b69c8c765ec0bdf657a5b1
nuke7 <gombos.marton@icloud.com>
2021-08-31 22:50:45 +0200

    up to date with master / custom fn

--- pages/purchases/index.vue
+++ pages/purchases/index.vue
@@ -47,6 +47,14 @@ export default {
         field: 'firstName',
         title: 'Keresztnév'
       }, {
+        customFn: (data) => {
+          const statuses = {
+            payed: 'Fizetve',
+            ordered: 'Megrendelve',
+            shipped: 'Kiszállítva'
+          }
+          return statuses[data.status]
+        },
         field: 'status',
         title: 'Vásárlás státusza'
       }, {
